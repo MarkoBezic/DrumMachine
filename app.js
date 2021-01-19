7a6b8cd24ba35e65f04afcba59a5eecd415826e4
markobezic <marko.bezic@gmail.com>
2021-01-19 14:25:02 -0500

    added functionality to play sound when button clicked or pressed key on keyboard. display updates with corresponding sound being played

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  $(".drum-pad").click((e) => {
+    $(e.target).children()[0].play();
+    console.log($(e.target)[0].id);
+    switch ($(e.target)[0].id) {
+      case "heater1":
+        $("#display").html("Heater 1");
+        break;
+      case "heater2":
+        $("#display").html("Heater 2");
+        break;
+      case "heater3":
+        $("#display").html("Heater 3");
+        break;
+      case "heater4":
+        $("#display").html("Heater 4");
+        break;
+      case "clap":
+        $("#display").html("Clap");
+        break;
+      case "openhh":
+        $("#display").html("Open HH");
+        break;
+      case "kicknhat":
+        $("#display").html("Kick n'Hat");
+        break;
+      case "kick":
+        $("#display").html("Kick");
+        break;
+      case "closedhh":
+        $("#display").html("Closed HH");
+        break;
+      default:
+        console.log("default case");
+    }
+  });
+  $(document).keydown((e) => {
+    switch (e.keyCode) {
+      case 81:
+        $("#Q")[0].play();
+        $("#display").html("Heater 1");
+        break;
+      case 87:
+        $("#W")[0].play();
+        $("#display").html("Heater 2");
+        break;
+      case 69:
+        $("#E")[0].play();
+        $("#display").html("Heater 3");
+        break;
+      case 65:
+        $("#A")[0].play();
+        $("#display").html("Heater 4");
+        break;
+      case 83:
+        $("#S")[0].play();
+        $("#display").html("Clap");
+        break;
+      case 68:
+        $("#D")[0].play();
+        $("#display").html("Open HH");
+        break;
+      case 90:
+        $("#Z")[0].play();
+        $("#display").html("Kick n'Hat");
+        break;
+      case 88:
+        $("#X")[0].play();
+        $("#display").html("Kick");
+        break;
+      case 67:
+        $("#C")[0].play();
+        $("#display").html("Closed HH");
+        break;
+      default:
+        console.log("Key pressed");
+    }
+  });
+});
